776e15397855166de001bc8a2421a730720b1d3e
muberracavli <muberracav@hotmail.com>
2020-01-08 20:28:21 -0500

    Acceptance criteria

---
+++ src/SoftSkillsHomework/AcceptanceCriteria.java
@@ -0,0 +1,28 @@
+package SoftSkillsHomework;
+/*
+ Acceptance criteria are the “conditions that a software product must satisfy to be accepted by a user,
+ customer or other stakeholders.
+ Most teams lean towards two specific types: rule-oriented or scenario-oriented.
+ Rule-oriented requirements are straight-forward. They list out observable outcomes.
+ “Display statement balance upon successful authentication.”
+ On the other hand, scenario-oriented criteria tend to follow the “Given…When…Then…” template.
+
+ Benefits of using acceptance criteria;
+ get the team to think through how a feature or piece of functionality will work from the user’s perspective
+ remove ambiguity from requirements
+ form the tests that will confirm that a feature or piece of functionality is working and complete.
+
+ */
+public class AcceptanceCriteria {
+    /*
+
+    Acceptance Criteria
+    1- "write your pin" is displayed
+    2- once users write the pin correctly , user navigated to "get cash" window.
+    3- if the users tries to withdraw $100 but the balance is under $100 ,user is navigated to "not authorize " window.
+    4- clicking the "not authorize " user is navigated back to "get cash " window.
+    5- when the user navigated to "get cash " window has to write the pin again.
+
+     */
+
+}
